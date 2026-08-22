8a62204416210221e2249714961c9d305a7136ad
BitRanger <63799660+b1tranger@users.noreply.github.com>
2026-08-22 16:13:45 +0600

    Update search-courses.js

--- js/search-courses.js
+++ js/search-courses.js
@@ -111,7 +111,7 @@ const courses = [
     // [ CSE ] { code: "", title: "", department: "Dept: CSE", driveLink: "" },
     //
     { code: "CSE351 + CSE352", title: "Compiler (+lab)", department: "Dept: CSE", driveLink: "https://drive.google.com/drive/folders/1nPDQAnm5fQVTLOxGP7NqCOcSwGGd3Bem?usp=drive_link" },
-    { code: "CSE357 + CSE358", title: "Computer Graphics and Multimedia (+Lab) - CG", department: "Dept: CSE", driveLink: "https://drive.google.com/drive/folders/138zkPmC6GXxJOPXdUb3zj4-tdqIT_ix9?usp=drive_link" },
+    { code: "CSE357 + CSE358", title: "Computer Graphics and Multimedia (+Lab) - CGM", department: "Dept: CSE", driveLink: "https://drive.google.com/drive/folders/138zkPmC6GXxJOPXdUb3zj4-tdqIT_ix9?usp=drive_link" },
     { code: "GED 115", title: "Industrial and Operation Management - IOM", department: "Dept: CSE", driveLink: "https://drive.google.com/drive/folders/1YstV74bC9Vy9VAkfCTpqILzA0jL2DYXd?usp=drive_link" },
     { code: "CSE359 + CSE360", title: "Microprocessors and Microcontrollers (+Lab) - MM", department: "Dept: CSE", driveLink: "https://drive.google.com/drive/folders/1snMh8qTLGLy5eqR5FBTVFhDAJYNITBqu?usp=drive_link" },
     { code: "CSE355 + CSE356", title: "Software Engineering and System Analysis (+lab) - SESA", department: "Dept: CSE", driveLink: "https://drive.google.com/drive/folders/1Sk3cJwYA0slESsWxa0Xvllerh40iTP-1?usp=drive_link" },
